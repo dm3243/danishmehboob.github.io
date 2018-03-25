e7234aa62e37c064d5ec1122367f41c3a04db401
Danish Mehboob <danishmehboob@Danishs-MacBook-Pro.local>
2018-03-25 00:41:56 -0400

    including modal functionality

--- scripts.js
+++ scripts.js
@@ -1,25 +1,17 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+$(function () {
+    $("#buttonClick").click(function() {
+        $("#DivPopUp").dialog({
+            title = "jquery modal",
+            width:430,
+            height:200,
+            modal: true,
+            buttons: {
+                Close:
+                function(){
+                    $(this).dialog('close');
+                }
+            }
+        });
+    });s
+})
