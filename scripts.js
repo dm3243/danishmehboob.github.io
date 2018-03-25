d94ba5e727d01ede656ee7673f44f505dd8442a7
Danish Mehboob <danishmehboob@Danishs-MacBook-Pro.local>
2018-03-25 12:39:44 -0400

    including modal functionality

--- scripts.js
+++ scripts.js
@@ -8,8 +8,10 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+window.onload = function() {
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
