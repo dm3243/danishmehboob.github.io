e6a62d35d60fb7d3ca0b3327716f5fa3f8b24f92
Danish Mehboob <danishmehboob@Danishs-MacBook-Pro.local>
2018-03-25 19:49:29 -0400

    including modal functionality

--- scripts.js
+++ scripts.js
@@ -4,12 +4,22 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("button");
 
+// Adding an event listner for clicking on button to open modal
+btn.addEventListener('click',openModal)
+
+// Function to open the modal
+function openModal(){
+    modal.style.display = 'block';
+}
+
+/*
 // When the user clicks on the button, open the modal 
 window.onload = function() {
     btn.onclick = function() {
         modal.style.display = "block";
     }
 }
+*/
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
