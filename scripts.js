a87190af84f1ad8c45d158b1d1b0d1f73bd9fb0c
Danish Mehboob <danishmehboob@Danishs-MacBook-Pro.local>
2018-03-25 12:08:03 -0400

    including modal functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById('firstmodal');
+var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("bttn");
