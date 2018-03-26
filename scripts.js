d4da792b2083a55cf77dc3394a3869739b964541
Danish Mehboob <danishmehboob@cnnny-mbair010.turner.com>
2018-03-26 16:20:28 -0400

    testing

--- scripts.js
+++ scripts.js
@@ -4,11 +4,16 @@ var modal = document.getElementsByClassName('modal');
 // Get the button that opens the modal
 var btn = document.getElementsByClassName("modalButton");
 
+for (let i=0; i<btn.length; i++){
+    btn[i].onclick = function() {
+        modal.style.display = 'block';
+    }
+}
 /* 
 Adding an event listner for clicking on button to open modal
 btn[0].addEventListener('click',openModal)
 */
-
+/*
 //to add an event listner to all the elements, need a for loop
 for (var i=0; i<btn.length; i++) {
     btn[i].addEventListener('click',openModal)
@@ -16,17 +21,21 @@ for (var i=0; i<btn.length; i++) {
 
 // Function to open the modal
 function openModal(){
-    modal[0].style.display = 'block';
+    console.log('hello')
+    //modal[0].style.display = 'block';
 }
+*/
 
-/*
 //My new method to try
 //based on forloop and function within a function as seen right below,
 //but does not work properly.
+/*
 for (var i=0; i<btn.length; i++) {
     btn[i].onclick = function() {
+        console.log(this)
         modal[i].style.display = "block";
-    }
+        console.log(i)
+    } 
 }
 */
 
