b7d47064b29722f47fac9e48972192406d61edc1
Jake Foraker <jakeforaker83@gmail.com>
2018-03-26 16:32:32 -0400

    stepping through the answers, still not perfect but works (#1)

--- scripts.js
+++ scripts.js
@@ -1,69 +1,37 @@
-// Get the modal
-var modal = document.getElementsByClassName('modal');
+// Get the modals
+var modals = document.querySelectorAll('.modal');
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("modalButton");
+var btns = document.querySelectorAll('.modalButton');
 
-for (let i=0; i<btn.length; i++){
-    btn[i].onclick = function() {
-        modal.style.display = 'block';
-    }
-}
-/* 
-Adding an event listner for clicking on button to open modal
-btn[0].addEventListener('click',openModal)
-*/
-/*
-//to add an event listner to all the elements, need a for loop
-for (var i=0; i<btn.length; i++) {
-    btn[i].addEventListener('click',openModal)
+//to add an event listner to all the elements, need a for loop or forEach iterator
+btns.forEach(function(button, index) {
+  setUpListener(index);
+});
+
+function setUpListener(index) {
+  btns[index].addEventListener('click', function(event) {
+    openModal(index);
+  });
 }
 
 // Function to open the modal
-function openModal(){
-    console.log('hello')
-    //modal[0].style.display = 'block';
+function openModal(index) {
+  // check if exists first
+  modals[index].style.display = 'block';
 }
-*/
-
-//My new method to try
-//based on forloop and function within a function as seen right below,
-//but does not work properly.
-/*
-for (var i=0; i<btn.length; i++) {
-    btn[i].onclick = function() {
-        console.log(this)
-        modal[i].style.display = "block";
-        console.log(i)
-    } 
-}
-*/
-
-/* This was the old method I used to open model...
-// When the user clicks on the button, open the modal 
-window.onload = function() {
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-}
-*/
-
-/*This portion of the code seems to break something...
-// Rewriting function to be two parts - event listener + function
-
-window.addEventListener('click',closeModal)
-
-function closeModal(event){
-    if(event.target == modal){
-        modal.style.display = 'none';
-    } 
-}
-*/
-
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal[0]) {
-        modal[0].style.display = "none";
-    }
-}
+  if (event.target.className === 'modal') {
+    modals.forEach(function(el) {
+      el.style.display = 'none';
+    });
+  }
+
+  //possibly more elegant solution:
+  // get the current element's number
+  // var myNumber = event.target.?something?;
+  // var myModal = document.querySelector(`modal-${number}`);
+  // target the modal with that number here
+};
